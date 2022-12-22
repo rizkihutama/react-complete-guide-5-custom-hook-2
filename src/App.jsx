04b8eca04b0b52b3470c173aabeecf34b9876a4a
rizkihutama <rizkihutama69@gmail.com>
2022-12-22 19:47:28 +0700

    step 1: Building custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
@@ -44,10 +44,10 @@ function App() {
   };
 
   return (
-    <React.Fragment>
+    <>
       <NewTask onAddTask={taskAddHandler} />
       <Tasks items={tasks} loading={isLoading} error={error} onFetch={fetchTasks} />
-    </React.Fragment>
+    </>
   );
 }
 
